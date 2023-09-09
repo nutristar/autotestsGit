ac6c5f792a768aaa194a818a8f5ed990540b444b
nutristar <deniszmail@gmail.com>
2023-09-09 18:54:24 +0200

    4rd commit

--- ooo.py
+++ ooo.py
@@ -1,1 +1,22 @@
-print("eto  deystvie iz ooo.py")
+def ooo(param):
+    # ваш код
+    print(param)
+    print("eto  deystvie iz ooo.py  !!!  ooo  iz ooo.py")
+
+def another_function(param):
+    # какой-то другой код
+    print(f"Another function: {param}")
+
+if __name__ == "__main__":
+    import sys
+    if len(sys.argv) > 2:
+        function_name = sys.argv[1]
+        param = sys.argv[2]
+        if function_name == "ooo":
+            ooo(param)
+        elif function_name == "another_function":
+            another_function(param)
+
+
+
+

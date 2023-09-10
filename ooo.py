dabdbcce2c044c4bdacf084b714c6ee0233ebd65
nutristar <deniszmail@gmail.com>
2023-09-10 17:01:50 +0200

    10ts commit223344

--- ooo.py
+++ ooo.py
@@ -9,7 +9,7 @@ def ooo(param1, param2):
     print(f"Deutcshland -- mein herz in flamen {x}")
     print("Willst zu liebst du und verdamen")
     print("Deutcshland -- dein atem ist kalt")
-    print("so jung und doch so alt!!!   !!")
+    print("so jung und doch so alt!!!     !!")
 
 
 def another_function(param):

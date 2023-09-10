e7dc0855fd0f5587f964f3b4cfc87e2cf7a6fd68
nutristar <deniszmail@gmail.com>
2023-09-10 15:41:18 +0200

    9ts commi666777

--- ooo.py
+++ ooo.py
@@ -1,31 +1,32 @@
+import time
+from datetime import datetime
+
+
 def ooo(param1, param2):
     # ваш код
-    print(f" {str(param1)}  я  {param2}")
-
-    x=str(param1)
+    print(f"{str(param1)} я {param2}")
+    x = str(param1)
     print(f"Deutcshland -- mein herz in flamen {x}")
     print("Willst zu liebst du und verdamen")
     print("Deutcshland -- dein atem ist kalt")
     print("so jung und doch so alt!!!!!")
 
+
 def another_function(param):
     # какой-то другой код
     print(f"Another function: {param}")
 
+
 if __name__ == "__main__":
     import sys
 
-    if __name__ == '__main__':
-        function_name = sys.argv[1]
+    function_name = sys.argv[1]
+    if function_name == 'ooo':
         param1 = sys.argv[2]
         param2 = sys.argv[3]
-
-        if function_name == 'ooo':
-            ooo(param1, param2)
-
-        elif function_name == "another_function":
-            another_function(param)
-
+        ooo(param1, param2)
+    elif function_name == "another_function":
+        another_function(datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
 
 
 
